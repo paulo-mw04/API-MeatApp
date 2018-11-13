6e04bf7a56b55eea09b55cc30c01c985605dd824
paulo-mw04 <45005667+paulo-mw04@users.noreply.github.com>
2018-11-13 11:50:49 -0200

    Add files via upload

---
+++ dist/users/users.router.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const model_router_1 = require("../common/model-router");
+const users_model_1 = require("./users.model");
+class UsersRouter extends model_router_1.ModelRouter {
+    constructor() {
+        super(users_model_1.User);
+        this.on('beforeRender', document => {
+            document.password = undefined;
+            //delete document.password
+        });
+    }
+    applyRoutes(application) {
+        application.get('/users', this.findAll);
+        application.get('/users/:id', [this.validateId, this.findById]);
+        application.post('/users', this.save);
+        application.put('/users/:id', [this.validateId, this.replace]);
+        application.patch('/users/:id', [this.validateId, this.update]);
+        application.del('/users/:id', [this.validateId, this.delete]);
+    }
+}
+exports.usersRouter = new UsersRouter();
